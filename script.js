cdef64a8816a5e0520e7e7415f83c2b1b8197351
LeJz0 <117210044+Lejz0@users.noreply.github.com>
2023-02-26 23:57:37 +0100

    Added color picker

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ function setupGrid() {
             var grid = document.getElementById("container");
             newDiv.style.cssText = "border : 1px solid black; height: 30px; width: 30px"
             newDiv.addEventListener("mouseover", function (){
-                this.style.cssText = "background-color : yellow;";
+                this.style.cssText = "background-color: " + getColor();
             });
             grid.appendChild(newDiv);
     }
 }
 
+function getColor(){
+    var color = document.getElementById("color").value;
+    return color;
+}
 window.onload = setupGrid;
 
